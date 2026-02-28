a0ecc6fe849ac70f5b896f3a36191023a7eedc34
Mithun Biswas <mithunbiswas.me@gmail.com>
2026-02-28 11:10:26 +0600

    updates codes ..

--- src/components/shared/BrandCard.jsx
+++ src/components/shared/BrandCard.jsx
@@ -30,7 +30,7 @@ const BrandCard = ({ brand }) => {
         />
       </div>
 
-      <span className="text-sm text-gray-700 font-medium">{brand.name}</span>
+      <span className="text-sm text-gray-700 font-medium">{brand.name} Name</span>
     </Link>
   );
 };
